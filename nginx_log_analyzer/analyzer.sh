cde32dac364e3095e6e07db79090a3699838f37f
klim <klim@theklim.com>
2025-12-06 20:21:18 +0100

    top 5 request and response status is done by analogy. the input simple validation of amount of parameters is done

--- nginx_log_analyzer/analyzer.sh
+++ nginx_log_analyzer/analyzer.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+if [ "$#" -lt 1 ]; then
+    echo "no log file given"
+    exit 1
+elif [ "$#" -ge 2 ]; then
+    echo "too many logs files"
+    exit 1
+elif [ ! -f "$1" ]; then
+     echo "path should be a file not a directory!"
+     exit 1
+else
+    echo "the log file is $1"
+fi
 
+echo "Top 5 IP addresses with the most requests:"
 awk -F ' ' '{print $1}' "$1" | sort | uniq --count | sort -gr | head -5
+
+echo "Top 5 most requested paths:"
+awk -F ' ' '{print $6,$7}' "$1" | sort | uniq --count | sort -gr | head -5
+
+echo "Top 5 response status codes:"
+awk -F ' ' '{print $9}' "$1" | sort | uniq --count | sort -gr | head -5
