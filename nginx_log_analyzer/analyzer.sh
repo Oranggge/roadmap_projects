6fccd5d585b6dd8deac7d479f8d1df41bff2eb79
klim <klim@theklim.com>
2025-12-06 20:33:17 +0100

    changed the output of most requested path to ally with the exercise output, added a hint how to analyze the "-" response code

--- nginx_log_analyzer/analyzer.sh
+++ nginx_log_analyzer/analyzer.sh
@@ -17,7 +17,10 @@ echo "Top 5 IP addresses with the most requests:"
 awk -F ' ' '{print $1}' "$1" | sort | uniq --count | sort -gr | head -5
 
 echo "Top 5 most requested paths:"
-awk -F ' ' '{print $6,$7}' "$1" | sort | uniq --count | sort -gr | head -5
+awk -F ' ' '{print $7}' "$1" | sort | uniq --count | sort -gr | head -5
 
 echo "Top 5 response status codes:"
 awk -F ' ' '{print $9}' "$1" | sort | uniq --count | sort -gr | head -5
+
+# to analyze "-" response status following command can be used:
+# awk '{if ($9 ~ "-") print $0 }' nginx-access.log
